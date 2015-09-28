abea32650aaba95b89fc1a925a208b50071e44ce
Daniel LaCosse <daniel.lacosse@pbb.me>
2015-09-27 21:48:46 -0400

    psql

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,27 @@
 
 ActiveRecord::Schema.define(version: 20150926225705) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blueprint_holds", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "blueprints", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "requirement_1"
+    t.string   "requirement_1_raw_value"
+    t.string   "requirement_2"
+    t.string   "requirement_2_raw_value"
+    t.string   "requirement_3"
+    t.string   "requirement_3_raw_value"
+    t.string   "requirement_4"
+    t.string   "requirement_4_raw_value"
+    t.string   "requirement_5"
+    t.string   "requirement_5_raw_value"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "game_states", force: :cascade do |t|
@@ -34,17 +47,10 @@ ActiveRecord::Schema.define(version: 20150926225705) do
     t.datetime "updated_at",    null: false
   end
 
-  add_index "game_states", ["holdable_type", "holdable_id"], name: "index_game_states_on_holdable_type_and_holdable_id"
+  add_index "game_states", ["holdable_type", "holdable_id"], name: "index_game_states_on_holdable_type_and_holdable_id", using: :btree
 
   create_table "games", force: :cascade do |t|
     t.integer  "guid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "games_players", force: :cascade do |t|
-    t.integer  "player_id"
-    t.integer  "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -69,13 +75,8 @@ ActiveRecord::Schema.define(version: 20150926225705) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "players", ["email"], name: "index_players_on_email", unique: true
-  add_index "players", ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
-
-  create_table "players_games", force: :cascade do |t|
-    t.integer "player_id"
-    t.integer "game_id"
-  end
+  add_index "players", ["email"], name: "index_players_on_email", unique: true, using: :btree
+  add_index "players", ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true, using: :btree
 
   create_table "scrap_holds", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -83,6 +84,9 @@ ActiveRecord::Schema.define(version: 20150926225705) do
   end
 
   create_table "scraps", force: :cascade do |t|
+    t.string   "name"
+    t.string   "class"
+    t.integer  "raw_value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
