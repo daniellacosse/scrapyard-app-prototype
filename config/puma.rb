8b87ed82d3fc1a30601b2f6ac7483d53b16ac6d2
Daniel LaCosse <d@niellacos.se>
2016-01-05 23:54:36 -0500

    16 threads, why the hell not

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers Integer(ENV["WEB_CONCURRENCY"] || 4)
-threads_count = Integer(ENV["MAX_THREADS"] || 8)
+threads_count = Integer(ENV["MAX_THREADS"] || 16)
 threads threads_count, threads_count
 
 preload_app!
