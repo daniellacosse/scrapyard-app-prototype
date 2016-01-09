75151349caddb28ed13cc00021d4f8a1a4f21d20
Daniel LaCosse <d@niellacos.se>
2016-01-09 14:44:39 -0500

    switch to rediscloud

--- config/puma.rb
+++ config/puma.rb
@@ -12,8 +12,5 @@ on_worker_boot do
   ActiveRecord::Base.establish_connection
 
   # Worker specific setup for Rails 4.1+
-  redis_connections_per_process = Integer(ENV["REDIS_CONNS_PER_PROCESS"] || 6)
-  $redis = ConnectionPool.new(size: redis_connections_per_process) do
-    Redis.new(url: ENV["REDIS_URL"] || "redis://localhost:6379/0")
-  end
+  $redis = Redis.new(url: ENV["REDISCLOUD_URL"] || "redis://localhost:6379/0")
 end
