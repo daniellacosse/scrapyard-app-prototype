83abfa965832f78458453aa49c88014fdb70152c
Daniel LaCosse <d@niellacos.se>
2016-01-13 20:01:38 -0500

    should do everything except messages

--- app/controllers/game_states_controller.rb
+++ app/controllers/game_states_controller.rb
@@ -7,59 +7,17 @@ ALL_STATES = %w(
 class GameStatesController < ApplicationController
   include ActionController::Live
   before_action :authenticate_player!
-  # before_action :ensure_game_started, only: [
-  #   :draw, :trade, :build, :end_turn, :ready
-  # ]
-  before_action :set_state, only: [
-    :show, :edit, :update, :destroy, :draw,
-		:trade, :build, :sell, :end_turn, :ready,
-    :discard_raw
-  ]
-
-  before_action :close_db_connection, only: :show
+  before_action :set_state
 
   def show
-    respond_to do |format|
-      format.html { render :show }
-
-      format.json do
-        response.headers['Content-Type'] = 'text/event-stream'
-
-        r = $redis || Redis.new( url: ENV["REDISCLOUD_URL"] || "redis://127.0.0.1:6379/0")
-        sub_string = "stream#{@game_state.id}"
-        @last_active = Time.zone.now
-
-        begin
-          r.subscribe sub_string do |on|
-            puts "#{current_player.email} Connected!"
-
-            on.message do |_event, data|
-              response.stream.write "event: update\n"
-              response.stream.write "data: #{data}\n\n"
-            end
-          end
-        rescue IOError
-          puts "IOError"
-        rescue ClientDisconnected
-          puts "ClientDisconnected"
-        rescue ActionController::Live::ClientDisconnected
-          puts "Live::ClientDisconnected"
-        ensure
-          puts "<<< STREAM IS KILL >>>\n"
-
-          r.unsubscribe sub_string
-          r.quit
-          response.stream.close
-        end
-      end
-    end
+    render :show
   end
 
   # POST /game_states/:id/discard_raw
   def discard_raw
-    success = @game_state.update raw: @game_state.raw - params[:raw_amount].to_i
+    @game_state.update raw: @game_state.raw - params[:raw_amount].to_i
 
-    handle_response success, ["raw"]
+    render :show
   end
 
   # POST /game_states/:id/draw/:card_type
@@ -70,35 +28,13 @@ class GameStatesController < ApplicationController
       hold = ScrapHold.create(scrap_id: card_id)
       @game_state.scrap_holds << hold
 
-      handle_response @game_state.save, [ "scraps" ] do
-        scrap = hold.scrap
-
-        @game_state.siblings.each do |sibling|
-          next if sibling.id == @game_state.id
-          matches = sibling.blueprints.to_a.select do |bp|
-            !sibling.holds?(scrap) && bp.requires?(scrap)
-          end
-
-          if matches.length > 0
-            alert_text = <<-HEREDOC
-              #{@game_state.player.email} just drew a(n) #{scrap.name}!
-              (You'll need one for: #{matches.map(&:name).join(', ')}.)
-            HEREDOC
-
-            send_alert to: sibling, with: alert_text
-          end
-        end
-      end
+      @game_state.save
+      render :show
     elsif card_type == "blueprint" && !!card_id && card_id != 0
       @game_state.blueprint_holds << BlueprintHold.create(blueprint_id: card_id)
 
-      handle_response @game_state.save, [ "blueprints", "available_blueprints" ] do
-        @game_state.siblings.each do |sibling|
-          next if sibling.id == @game_state.id
-
-          publish_data sibling, [ "available_blueprints" ]
-        end
-      end
+      @game_state.save
+      render :show
     else
       flash[:error] = "Card type drawn (#{card_type}) invalid!"
     end
@@ -108,34 +44,32 @@ class GameStatesController < ApplicationController
   def sell
     @scrap_hold = ScrapHold.find params[:scrap_hold_id]
 
-    handle_response @scrap_hold.sell, ["raw", "scraps"]
+    @scrap_hold.sell
+
+    render :show
   end
 
   # POST /game_states/:id/build/:blueprint_id
   def build
     @blueprint_hold = BlueprintHold.find params[:blueprint_id]
-    success = false
     tribute = JSON.parse(params[:build_blob]) if params[:build_blob]
 
-    # transaction do
-      if !!tribute
-        success = @game_state.update(raw: @game_state.raw - tribute["raw"].to_i)
-        success &= tribute["scraps"].map do |scrap_name|
-          @game_state.scrap_holds
-            .to_a
-            .find { |sh| sh.scrap.name == scrap_name }
-            .destroy
-        end.all?
-      end
+    if !!tribute
+      @game_state.update(raw: @game_state.raw - tribute["raw"].to_i)
+      tribute["scraps"].map do |scrap_name|
+        @game_state.scrap_holds
+          .to_a
+          .find { |sh| sh.scrap.name == scrap_name }
+          .destroy
+      end.all?
+    end
 
-      success &= @blueprint_hold.destroy
-      success &= ModuleHold.create(
-        scrapper_module_id: @blueprint_hold.blueprint.scrapper_module.id,
-        game_state_id: @game_state.id
-      )
-    # end
+    @blueprint_hold.destroy
+    ModuleHold.create(
+      scrapper_module_id: @blueprint_hold.blueprint.scrapper_module.id,
+      game_state_id: @game_state.id
+    )
 
-    publish_data @game_state, ALL_STATES
     render :show
   end
 
@@ -148,11 +82,11 @@ class GameStatesController < ApplicationController
     if all_ready
       @game_state.siblings.each { |state| publish_data state, ALL_STATES }
 
-      render json: @game_state, status: :updated
+      render :show
     else
       @game_state.siblings.each { |state| publish_data state, [ "players" ] }
 
-      render json: @game_state, status: :updated
+      render :show
     end
   end
 
@@ -168,14 +102,15 @@ class GameStatesController < ApplicationController
       end
     end
 
-    success = @game_state.update(is_my_turn: false) && @next_player_state.update(is_my_turn: true)
+    @game_state.update(is_my_turn: false)
+    @next_player_state.update(is_my_turn: true)
 
     @game_state = GameState.find params[:id]
     @next_player_state = GameState.find @next_player_state.id
 
     @game_state.siblings.each { |state| publish_data state, [ "players" ] }
 
-    render json: @game_state, status: :updated
+    render :show
   end
 
   def destroy
@@ -190,22 +125,5 @@ class GameStatesController < ApplicationController
 
   def set_state
     @game_state = GameState.find params[:id]
-  end
-
-  def handle_response(no_error, states = ALL_STATES, &block)
-		set_state # awkwaaarddd
-
-    if no_error
-      publish_data @game_state, states
-      block.call if block
-
-      render json: @game_state, status: :updated
-    else
-      render json: @game_state.errors, status: :unprocessable_entity
-    end
-  end
-
-  def close_db_connection
-    ActiveRecord::Base.connection_pool.release_connection
   end
 end
