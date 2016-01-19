0a5dd5fb4dcce9d9016a2f68b05780f2218e11b4
Daniel LaCosse <d@niellacos.se>
2016-01-18 23:29:34 -0500

    build init

---
+++ app/controllers/module_holds_controller.rb
@@ -0,0 +1,35 @@
+require "json"
+
+class ModuleHoldsController < ApplicationController
+  before_action :set_blueprint_hold, only: [:new, :create]
+
+  def new
+    @module_hold = ModuleHold.new(
+      game_state_id: @game_state.id,
+      scrapper_module_id: @blueprint_hold.blueprint.scrapper_module.id
+    )
+  end
+
+  def create
+    if @game_state.discard_resources JSON.parse(hold_params[:build])
+      ModuleHold.create(
+        game_state_id: @game_state.id,
+        scrapper_module_id: @blueprint_hold.blueprint.scrapper_module.id
+      )
+    else
+      flash[:error] = "Something went wrong. You probably didn't have the right resources."
+    end
+
+    render @game_state
+  end
+
+  private
+  def hold_params
+    params.require(:module_hold).permit(:build)
+  end
+
+  def set_blueprint_hold
+    @blueprint_hold = BlueprintHold.find(params[:blueprint_hold_id])
+    @game_state = @blueprint_hold.game_state
+  end
+end
--- app/models/blueprint_hold.rb
+++ app/models/blueprint_hold.rb
@@ -5,4 +5,64 @@ class BlueprintHold < ActiveRecord::Base
 	belongs_to :blueprint
 
 	validates_uniqueness_of :blueprint_id, scope: :game_id
+
+	def build_options
+		resource_pool = ResourcePool.new(game_state: game_state)
+
+		build_list resource_pool, blueprint.requirements, ResourcePool.new
+	end
+
+	private
+	def build_list(resource_pool, requirements, build_pool)
+		return build if resources.length <= 0
+
+		build_branches = []
+
+		requirements.first.options.each do |option|
+			option_pool = ResourcePool.new(option: option)
+
+			# close, but not quite. this doesn't do classes (that's really the kicker)
+			if resource_pool.contains? option_pool
+				build_branch = build_pool + option_pool
+				leftovers = resource_pool - option_pool
+
+				build_branches << build_list(
+					leftovers, requirements[1..-1], build_branch
+				)
+			end
+		end
+
+		build_branches.flatten
+	end
+end
+
+class ResourcePool
+	def initialize(params)
+		if params[:game_state]
+			@scraps = params[:game_state].scrap_holds.include(:scrap)
+			@raw = params[:game_state].raw
+		elsif params[:option]
+			@scraps = params[:option].scraps + params[:option].class_types
+		else
+			@scraps = params[:scraps] || []
+			@raw = params[:raw] || 0
+		end
+	end
+
+	def +(pool)
+		new(raw: raw + pool.raw, scraps: scraps + pool.scraps)
+	end
+
+	def -(pool)
+		new(raw: raw - pool.raw, scraps: scraps - pool.scraps)
+	end
+
+	def contains?(pool)
+		is_contain = raw > pool.raw
+
+		# still doesn't do class comparisons
+		is_contain &= pool.scraps.all? { |scrap| scraps.include? scrap }
+
+		is_contain
+	end
 end
