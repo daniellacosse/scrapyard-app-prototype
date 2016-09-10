fac34ecc114f3f32e6ccebd323526e44b8411bcb
Daniel LaCosse <d@niellacos.se>
2016-09-10 16:10:22 -0400

    ......

--- app/models/blueprint_hold.rb
+++ app/models/blueprint_hold.rb
@@ -9,7 +9,9 @@ class BlueprintHold < ActiveRecord::Base
 	def build_options
 		my_resource_pool = ResourcePool.new(game_state: game_state)
 
-		build_list my_resource_pool, blueprint.requirements.to_a
+		build_list(my_resource_pool, blueprint.requirements.to_a).select do |build|
+			build.met_requirements == blueprint.requirements.length
+		end
 	end
 
 	private
@@ -21,62 +23,141 @@ class BlueprintHold < ActiveRecord::Base
 		requirement = requirements.first
 		override_pool = ResourcePool.new(raw: requirement.override_value)
 
+		# check override
+		if resource_pool.raw.to_i >= override_pool.raw.to_i
+			build_branch = build_pool + override_pool
+			leftovers = resource_pool - override_pool
+
+			build_branch.met_requirements += 1
+
+			build_branches << build_list(
+				leftovers, requirements[1..-1], build_branch
+			)
+		end
+
 		requirement.options.each do |option|
-			option_pool = ResourcePool.new(option: option)
-
-			if resource_pool.contains? option_pool
-				build_branch = build_pool + option_pool
-				leftovers = resource_pool - option_pool
-
-				build_branches << build_list(
-					leftovers, requirements[1..-1], build_branch
-				)
+			class_list = [].tap do |_cl|
+				option.class_options.each do |cop|
+					cop.count.times { _cl << cop.class_type }
+				end
 			end
 
-			if resource_pool.contains? override_pool
-				build_branch = build_pool + override_pool
-				leftovers = resource_pool - override_pool
+			option_pool = ResourcePool.new(
+				scraps: option.scraps.to_a,
+				scrap_types: class_list
+			)
+
+			# check all fits
+			resource_pool.list_fits(option_pool).each do |option_fit|
+				build_branch = build_pool + option_fit
+				leftovers = resource_pool - option_fit
+
+				build_branch.met_requirements += 1
 
 				build_branches << build_list(
 					leftovers, requirements[1..-1], build_branch
 				)
 			end
 		end
+
+		byebug
 
 		build_branches.flatten
 	end
 end
 
 class ResourcePool
-	attr_reader :scraps, :raw
+	attr_accessor :met_requirements
+	attr_reader :scraps, :scrap_types, :raw
+
+	def self.permute_over_scrap_types(scrap_types, source_pool, build_pool = ResourcePool.new)
+		if scrap_types.length == 0
+			return [ build_pool ]
+		end
+
+		scrap_type = scrap_types.shift
+		scrap_type_branch = []
+
+		source_pool.scrap_type_matches(scrap_type).each do |matching_scrap|
+			matching_scrap_pool = ResourcePool.new(scraps: [ matching_scrap ])
+
+			next_pool = build_pool + matching_scrap_pool
+			remainder_pool = source_pool - matching_scrap_pool
+
+			next_pool.met_requirements += 1
+
+			scrap_type_branch << ResourcePool.permute_over_scrap_types(
+				scrap_types, remainder_pool, next_pool
+			)
+		end
+
+		byebug
+
+		scrap_type_branch.flatten
+	end
 
 	def initialize(params = {})
+		@met_requirements = params[:met_requirements] || 0
+
 		if params[:game_state]
-			@scraps = params[:game_state].scrap_holds.includes(:scrap).to_a
+			@scraps = params[:game_state].scrap_holds.map(&:scrap).to_a
+			@scrap_types = []
 			@raw = params[:game_state].raw
 		elsif params[:option]
-			@scraps = params[:option].scraps.to_a + params[:option].class_types.to_a
+			@raw = 0
+			@scraps = params[:option].scraps.to_a
+			params[:option].class_options.to_a.each do |class_option|
+				class_option.count.times { @scrap_types << class_option.class_type }
+			end
 		else
 			@scraps = params[:scraps] || []
+			@scrap_types = params[:scrap_types] || []
 			@raw = params[:raw] || 0
 		end
 	end
 
 	def +(pool)
-		ResourcePool.new(raw: raw.to_i + pool.raw.to_i, scraps: scraps + pool.scraps)
+		ResourcePool.new(
+			met_requirements: met_requirements,
+			raw: raw.to_i + pool.raw.to_i,
+			scrap_types: scrap_types + pool.scrap_types,
+			scraps: scraps + pool.scraps
+		)
 	end
 
 	def -(pool)
-		ResourcePool.new(raw: raw.to_i - pool.raw.to_i, scraps: scraps - pool.scraps)
+		ResourcePool.new(
+			met_requirements: met_requirements,
+			raw: raw.to_i - pool.raw.to_i,
+			scrap_types: scrap_types - pool.scrap_types,
+			scraps: scraps - pool.scraps
+		)
 	end
 
-	def contains?(pool)
-		is_contain = raw.to_i >= pool.raw.to_i
+	def scrap_type_matches(type)
+		self.scraps.select do |scrap|
+			scrap.class_types.to_a.map(&:name).include? type.name
+		end
+	end
 
-		# TODO: class comparisons, multiple of scrap/class
-		is_contain &= pool.scraps.all? { |scrap| scraps.map(&:scrap).include? scrap }
+	def list_fits(pool)
+		unless pool.scraps.all? { |scrap| scraps.include? scrap }
+			return []
+		end
 
-		is_contain
+		# recurse over scrap_types
+			# pass in scrap_types, truncated pool, ResourcePool w/ scraps
+		byebug
+		scrap_pool = ResourcePool.new(scraps: pool.scraps)
+		remainder = self - scrap_pool
+		ResourcePool
+			.permute_over_scrap_types(pool.scrap_types, remainder)
+			.select { |result_pool| result_pool.met_requirements == pool.scrap_types.length }
+			.map { |fit| fit + scrap_pool }
+	end
+
+	def empty?
+		scraps.length == 0 && raw == 0 && scrap_types.length == 0
 	end
 
 	# TODO: fix
